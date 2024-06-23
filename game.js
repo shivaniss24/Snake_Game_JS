f40732acd8814427fc04f9e4d982479fcf8d4501
Shivani Soni <eng.shivanisoni2405@gmail.com>
2024-06-23 23:26:25 +0530

    final commit

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import {update as updateSnake,draw as drawSnake, snake_speed,getSnakeHead,snakeIntersection} from './snake.js';
+import {update as updateSnake, draw as drawSnake, snake_speed,getSnakeHead,snakeIntersection} from './snake.js';
 import {update as updateFood, draw as drawFood} from './food.js'
 import { outSideGrid } from './grid.js';
 
